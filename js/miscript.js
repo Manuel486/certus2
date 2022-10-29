7bf1bd746c9cf2d2823c9da1b327cd4499712be7
Manuel486 <82180428+Manuel486@users.noreply.github.com>
2022-10-29 12:43:28 -0500

    segundo commit

--- js/miscript.js
+++ js/miscript.js
@@ -1,3 +1,17 @@
+
+/*
 function f1(){
     alert('Hola mundo')
+}
+*/
+
+/**
+ * Función que muestra un mensaje de alerta
+ * @param {string} msg mensaje a mostrar
+ * @since 1.0
+ * @version 2.0
+ */
+
+function f1(msg){
+    alert(msg);
 }
